7aa811dffaf4596873b1b306b99051494dc319d8
Aaliyah-Reid <aaliyahreid12345@gmail.com>
2024-06-15 23:31:47 -0500

    added more paintings

--- src/components/PaintingsData.jsx
+++ src/components/PaintingsData.jsx
@@ -13,6 +13,21 @@ export const paintings = [
       id: 3,
       title: "Untitled3",
       description: "5 in x 7 in, Acrylic on Canvas",
+    },
+    {
+      id: 4,
+      title: "Tweaked Femininity, 2019",
+      description: "40 x 62 cm, Acrylic on Canvas",
+    },
+    {
+      id: 5,
+      title: "Scarred by Pride, 2019",
+      description: "40 x 62 cm, Acrylic on Canvas",
+    },
+    {
+      id: 6,
+      title: "Nurtured by Hate, 2019",
+      description: "40 x 62 cm, Mixed Media",
     }
     
   ];
